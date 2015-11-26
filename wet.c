4828c83e19b20f131686e69098262b8f0903593b
Zichao Yan <yanzichao.cn@163.com>
2015-11-26 11:13:24 +0200

    Initial stage
    Now wet.c could be compiled. Next step is to implement each individual
    function

--- wet.c
+++ wet.c
@@ -1,4 +1,6 @@
 #include "wet.h"
+#include <stdio.h>
+#include "libpq-fe.h"
 
 int main(int argc, char* argv[]) {
 	char connect_param[200];
@@ -7,4 +9,57 @@ int main(int argc, char* argv[]) {
 		USERNAME, USERNAME, PASSWORD);
 	PGconn *conn = PQconnectdb(connect_param);
 	return 0;
+}
+
+void* addUser(const char*    name){
+
+}
+
+void* addUserMin(const char*    name){
+
+}
+
+void* removeUser(const char*    id){
+
+}
+
+void* addPhoto(const char*    user_id,
+	const char*    photo_id){
+
+}
+
+void* tagPhoto(const char*    user_id,
+	const char*    photo_id,
+	const char*    info){
+
+
+}
+
+void* photosTags(){
+
+}
+
+void* search(const char*    word){
+
+}
+
+void* commonTags(const char*    k){
+
+}
+
+void* mostCommonTags(const char*    k){
+
+}
+
+void* similarPhotos(const char*    k,
+	const char*    j){
+
+}
+
+void* autoPhotoOnTagOn(){
+
+}
+
+void* autoPhotoOnTagOFF(){
+
 }
